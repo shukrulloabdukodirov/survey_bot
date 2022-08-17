e18d770bfa9721c0310d7be0bdf748d578680f28
Shukrullo Abduqodirov <abduqodirov0101@gmail.com>
2022-08-17 13:14:40 +0500

    webhook actions

--- base/Application/Application/Http/Webhooks/SurveyWebhookHandler.php
+++ base/Application/Application/Http/Webhooks/SurveyWebhookHandler.php
@@ -12,8 +12,8 @@ class SurveyWebhookHandler extends \DefStudio\Telegraph\Handlers\WebhookHandler
     {
         Telegraph::message('hello world')
             ->keyboard(Keyboard::make()->buttons([
-                Button::make("🗑️ Delete")->action("delete")->param('id', $notification->id),
-                Button::make("📖 Mark as Read")->action("read")->param('id', $notification->id),
+                Button::make("🗑️ Delete")->action("delete")->param('id', "1"),
+                Button::make("📖 Mark as Read")->action("read")->param('id', "1"),
                 Button::make("👀 Open")->url('https://test.it'),
             ])->chunk(2))->send();
     }
