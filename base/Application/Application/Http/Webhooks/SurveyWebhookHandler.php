2a944ddc6a910418d8646149cd94223dd6bf9869
Shukrullo Abduqodirov <abduqodirov0101@gmail.com>
2022-08-17 19:14:31 +0500

    webhook actions

--- base/Application/Application/Http/Webhooks/SurveyWebhookHandler.php
+++ base/Application/Application/Http/Webhooks/SurveyWebhookHandler.php
@@ -42,7 +42,7 @@ class SurveyWebhookHandler extends \DefStudio\Telegraph\Handlers\WebhookHandler
     {
         $this->chat->message('<b>Assalomu alaykum </b>'.$this->message->from()->username().' Iltimos telefon raqamingizni bizga yuboring.')->replyKeyboard(ReplyKeyboard::make()
             ->row([
-                ReplyButton::make('Telefon raqamni yuborish')->requestContact()->width(0.1)
+                ReplyButton::make('Telefon raqamni yuborish')->requestContact()->action('salom')->param('id','suka')
             ])->chunk(3)->resize(false)->selective(true))
             ->send();
     }
