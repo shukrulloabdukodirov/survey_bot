08bb380c659849b8112502c585449db2d6c97977
BaxtiyorBohodirov <bohodirovbaxtiyor19@gmail.com>
2022-09-10 18:48:52 +0500

    last

--- base/Application/Application/Http/Webhooks/SurveyWebhookHandler.php
+++ base/Application/Application/Http/Webhooks/SurveyWebhookHandler.php
@@ -388,12 +388,12 @@ class SurveyWebhookHandler extends BaseWebHookHandler
     {
         $answers = json_decode($answers);
         Log::info($answers);
-        // foreach ($answers as $answer) {
-        //     $appAns = ApplicationAnswer::firstOrCreate(['application_id' => $this->application->id, 'question_id' => $answer->question_id]);
-        //     $appAns->update([
-        //         'question_answer_id' => isset($answer->answer_id) ? $answer->answer_id : null,
-        //         'answer_by_input' => isset($answer->answer_by_input) ? $answer->answer_by_input : null
-        //     ]);
-        // }
+        foreach ($answers as $answer) {
+            $appAns = ApplicationAnswer::firstOrCreate(['application_id' => $this->application->id, 'question_id' => $answer->question_id]);
+            $appAns->update([
+                'question_answer_id' => isset($answer->answer_id)&& is_numeric($answer->answer_id) ? $answer->answer_id : null,
+                'answer_by_input' => isset($answer->answer_by_input) ? $answer->answer_by_input : null
+            ]);
+        }
     }
 }
